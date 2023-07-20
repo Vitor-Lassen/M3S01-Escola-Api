57a0db5fbb1dd2e2bb11c554a555564617d2d65b
Vitor Lassen <vitor@lassen.com.br>
2023-07-19 21:49:02 -0300

    Moq

---
+++ Escola.API.Tests/Controller/BoletimControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Escola.API.Controllers; 
+using System.Threading.Tasks;
+using Moq;
+using Escola.API.Interfaces.Services;
+using Escola.API.Model;
+using Escola.API.DTO;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework.Constraints;
+using Newtonsoft.Json;
+
+namespace Escola.API.Tests.Controller
+{
+    public class BoletimControllerTest
+    {
+
+        [Test]
+        public void CriarBoletim_AdicionarBoletim_Sucesso()
+        {
+            //Arrange 
+
+            var boletimServiceMock = new Mock<IBoletimService>();
+            boletimServiceMock.Setup(x => x.Cadastrar(It.IsAny<Boletim>())).Returns(new Boletim() { Id = 10 });
+
+            var boletimController = new BoletimController(boletimServiceMock.Object);
+
+            var boletim = new BoletimDTO() { Data = new DateTime(2023, 07, 18), AlunoId = 5 };
+            var expectedBoletim = new BoletimDTO() { Data = new DateTime(2023, 07, 18), AlunoId = 5, Id = 10 };
+            var expectedStatus = 200;
+
+            //Act
+            var result = boletimController.Post(boletim, 5);
+
+            //ASSERTs
+            Assert.IsTrue(result is OkObjectResult);
+            var resultOk = (OkObjectResult)result;
+
+            Assert.AreEqual(expectedStatus, resultOk.StatusCode);
+
+            Assert.AreEqual(JsonConvert.SerializeObject(expectedBoletim), JsonConvert.SerializeObject(resultOk.Value));
+
+        }
+    }
+}
---
+++ Escola.API.Tests/Services/AlunoServicesTestsMoq.cs
@@ -0,0 +1,104 @@
+﻿using Escola.API.Exceptions;
+using Escola.API.Interfaces.Repositories;
+using Escola.API.Model;
+using Escola.API.Services;
+using Escola.API.Tests.Mocks.Reposiories;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Escola.API.Tests.Services
+{
+    public  class AlunoServicesTestsMoq
+    {
+        [Test]
+        public void Criar_AlunoNaoCadastrado_Successo()
+        {
+            //ARRANGE
+            var aluno = new Aluno() { Nome = "Vitor", Email = "vitor@email.com", Sobrenome = "Lassen" };
+            var expectedAluno = new Aluno() { Id = 10, Nome = "Vitor", Email = "vitor@email.com", Sobrenome = "Lassen" };
+
+            var alunoRepository = new AlunoRepositoryMock();
+
+            var alunoRepositoryMoq = new Mock<IAlunoRepository>();
+            alunoRepositoryMoq.Setup(x => x.EmailJaCadastrado("vitor@email.com")).Returns(false);
+            alunoRepositoryMoq.Setup(x => x.Inserir(aluno)).Returns(() => 
+                                                            {
+                                                                aluno.Id = 10;
+                                                                return aluno;
+                                                               });
+
+            var alunoService = new AlunoService(alunoRepositoryMoq.Object);
+
+            //ACT
+            var result = alunoService.Criar(aluno);
+
+            //Assert
+            Assert.AreEqual(JsonConvert.SerializeObject(expectedAluno), JsonConvert.SerializeObject(result));
+
+        }
+        [Test]
+        public void Criar_AlunoNaoCadastrado_Successo_ItsAny()
+        {
+            //ARRANGE
+            var aluno = new Aluno() { Nome = "Vitor", Email = "vitor@email.com", Sobrenome = "Lassen" };
+            var expectedAluno = new Aluno() { Id = 10, Nome = "Vitor", Email = "vitor@email.com", Sobrenome = "Lassen" };
+
+            var alunoRepository = new AlunoRepositoryMock();
+
+            var alunoRepositoryMoq = new Mock<IAlunoRepository>();
+            alunoRepositoryMoq.Setup(x => x.EmailJaCadastrado(It.IsAny<string>())).Returns(false);
+            alunoRepositoryMoq.Setup(x => x.Inserir(It.IsAny<Aluno>())).Returns(() =>
+            {
+                aluno.Id = 10;
+                return aluno;
+            });
+
+            var alunoService = new AlunoService(alunoRepositoryMoq.Object);
+
+            //ACT
+            var result = alunoService.Criar(aluno);
+
+            //Assert
+
+            Assert.AreEqual(JsonConvert.SerializeObject(expectedAluno), JsonConvert.SerializeObject(result));
+        }
+
+        [Test]
+        public void Criar_AlunoJaCadastrado_ErroDuplicidade()
+        {
+
+            // ARRANGE
+            var aluno = new Aluno() { Nome = "Camila", Email = "camila@email.com", Sobrenome = "Santos" };
+            var expectedMessage = "email já cadastrado";
+
+            var alunoRepositoryMoq = new Mock<IAlunoRepository>();
+            alunoRepositoryMoq.Setup(x => x.EmailJaCadastrado("camila@email.com")).Returns(true);
+
+            var alunoService = new AlunoService(alunoRepositoryMoq.Object);
+
+            //ACT
+            //Assert
+            var ex = Assert.Throws<RegistroDuplicadoException>(() => alunoService.Criar(aluno));
+            Assert.AreEqual(expectedMessage, ex.Message);
+
+        }
+        [Test]
+        public void Criar_AlunoJaCadastrado_ErroDuplicidade_ItsAny()
+        {
+            // ARRANGE
+            var aluno = new Aluno() { Nome = "Camila", Email = "camila@email.com", Sobrenome = "Santos" };
+            var expectedMessage = "email já cadastrado";
+
+            var alunoRepositoryMoq = new Mock<IAlunoRepository>();
+            alunoRepositoryMoq.Setup(x => x.EmailJaCadastrado(It.IsAny<string>())).Returns(true);
+
+            var alunoService = new AlunoService(alunoRepositoryMoq.Object);
+
+            //ACT
+            //Assert
+            var ex = Assert.Throws<RegistroDuplicadoException>(() => alunoService.Criar(aluno));
+            Assert.AreEqual(expectedMessage, ex.Message);
+        }
+    }
+}
